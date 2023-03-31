672d65034e71e0a339843b7c488b074e6271795d
Matthew Baggett <matthew@baggett.me>
2023-03-31 13:42:38 +0200

    initial commit

--- src/WatchCommand.php
+++ src/WatchCommand.php
@@ -3,11 +3,14 @@
 namespace ComposerWatcher;
 
 use Composer\Command\BaseCommand;
+use Spatie\Watcher\Watch;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\StringInput;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class WatchCommand extends BaseCommand
 {
+    public const CHECK_INTERVAL_SECONDS = 3;
     protected function configure(): void
     {
         $this->setName('watch');
@@ -21,7 +24,21 @@ class WatchCommand extends BaseCommand
             return 1;
         }
 
-        $output->writeln(sprintf("Found composer.lock: %s", md5_file($lockFile)));
+        // This solution requires chokidar and that depends on npm and friends...
+        #Watch::path($lockFile)->onAnyChange(function(string $type, string $path) use ($output){
+        #    $output->writeln("Running composer install");
+        #    if($type === Watch::EVENT_TYPE_FILE_UPDATED){
+        #        $this->getApplication()->doRun(new StringInput("install"), $output);
+        #    }
+        #})->start();
+
+        while(true) {
+            $md5 = md5_file($lockFile);
+            while ($md5 == md5_file($lockFile)) {
+                sleep(self::CHECK_INTERVAL_SECONDS);
+            }
+            $this->getApplication()->doRun(new StringInput("install"), $output);
+        }
 
         return 0;
     }
